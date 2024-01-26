1d091574fa68ed0e3a554a4b88a8ac0b090eac50
brianvikonura <vikonura@gmail.com>
2024-01-26 23:51:33 +0700

    Add register and upload avatar pages

--- pages/register.vue
+++ pages/register.vue
@@ -5,9 +5,33 @@
     ></div>
     <div class="flex items-center justify-center w-auto md:w-2/4 lg:w-2/3">
       <div class="w-full px-10 lg:w-1/2 lg:px-0">
-        <h2 class="mb-6 text-3xl font-normal text-white">
-          Sign In to Your Account
-        </h2>
+        <h2 class="mb-6 text-3xl font-normal text-white">Sign Up Account</h2>
+        <div class="mb-6">
+          <div class="mb-4">
+            <label class="block mb-3 text-lg font-normal text-white"
+              >Full Name</label
+            >
+            <input
+              type="text"
+              class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
+              placeholder="Write Your Name Here"
+              value="Brian Viko Nura"
+            />
+          </div>
+        </div>
+        <div class="mb-6">
+          <div class="mb-4">
+            <label class="block mb-3 text-lg font-normal text-white"
+              >Occupation</label
+            >
+            <input
+              type="text"
+              class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
+              placeholder="Write your occupation here"
+              value="Website Developer"
+            />
+          </div>
+        </div>
         <div class="mb-6">
           <div class="mb-4">
             <label class="block mb-3 text-lg font-normal text-white"
@@ -29,26 +53,25 @@
             <input
               type="password"
               class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
-              placeholder="Write your password here"
-              value="nasigorenglimaribbu"
+              placeholder="Type your password here"
+              value="github.com/Brianvikonura"
             />
           </div>
         </div>
         <div class="mb-6">
           <div class="mb-4">
             <button
-              @click="$router.push({ path: '/' })"
+              @click="$router.push({ path: '/upload' })"
               class="block w-full px-6 py-4 text-lg font-semibold text-white rounded-full bg-orange-button hover:bg-green-button"
             >
-              Sign In
+              Continue Sign Up
             </button>
           </div>
         </div>
         <div class="text-center">
           <p class="text-white text-md">
-            Don't have account?
-            <nuxt-link to="/register" class="no-underline text-orange-button"
-              >Sign Up</nuxt-link
+            Already have account?
+            <nuxt-link to="/login" class="no-underline text-orange-button">Sign In</nuxt-link
             >.
           </p>
         </div>
@@ -65,7 +88,7 @@ export default {
 
 <style scoped>
 .auth-background {
-  background-image: url("/sign-in-background.jpg");
+  background-image: url('/sign-up-background.jpg');
   background-position: center;
   background-size: cover;
 }
