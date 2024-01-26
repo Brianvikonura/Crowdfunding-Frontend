132b03b50c8e83a953946c223462daa75e042e9c
brianvikonura <vikonura@gmail.com>
2024-01-26 19:17:46 +0700

    Add Navbar, Footer, and CTA components

---
+++ components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="flex items-center">
+    <div style="height: 54px" class="pr-5">
+      <img src="/logo.svg" alt="logo" class="h-full" />
+    </div>
+    <ul class="flex items-center">
+      <li>
+        <nuxt-link class="px-4 py-3 text-lg text-white hover:text-teal-500" to="/"
+          >Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          class="px-4 py-3 text-lg text-white hover:text-teal-500"
+          to="/projects"
+          >Project</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          class="px-4 py-3 text-lg text-white hover:text-teal-500"
+          to="/features"
+          >Features</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          class="px-4 py-3 text-lg text-white hover:text-teal-500"
+          to="/story"
+          >Success Stories</nuxt-link>
+      </li>
+    </ul>
+    <ul class="flex items-center mt-2 ml-auto">
+      <li>
+        <nuxt-link
+          to="/register"
+          class="inline-block w-40 px-6 py-1 mr-4 text-lg font-light text-center text-white bg-transparent border border-white rounded-full hover:bg-white hover:bg-opacity-25">
+          Sign Up
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          to="/login"
+          class="inline-block w-40 px-6 py-1 text-lg font-light text-center text-white bg-transparent border border-white rounded-full hover:bg-white hover:bg-opacity-25">
+          My Account
+        </nuxt-link>
+      </li>
+    </ul>
+  </header>
+</template>
