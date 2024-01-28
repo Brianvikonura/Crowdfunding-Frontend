e7a5fc2d15778d3d59d78b7fcd3a6d209a57f205
brianvikonura <vikonura@gmail.com>
2024-01-28 18:53:12 +0700

    API integration: register and upload avatar pages

--- pages/register.vue
+++ pages/register.vue
@@ -13,9 +13,9 @@
             >
             <input
               type="text"
+              v-model="register.name"
               class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
               placeholder="Write Your Name Here"
-              value="Brian Viko Nura"
             />
           </div>
         </div>
@@ -26,9 +26,9 @@
             >
             <input
               type="text"
+              v-model="register.occupation"
               class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
               placeholder="Write your occupation here"
-              value="Website Developer"
             />
           </div>
         </div>
@@ -39,9 +39,9 @@
             >
             <input
               type="email"
+              v-model="register.email"
               class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
               placeholder="Write your email address here"
-              value="example@gmail.com"
             />
           </div>
         </div>
@@ -52,16 +52,17 @@
             >
             <input
               type="password"
+              v-model="register.password"
               class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
               placeholder="Type your password here"
-              value="github.com/Brianvikonura"
+              @keyup.enter="userRegister()"
             />
           </div>
         </div>
         <div class="mb-6">
           <div class="mb-4">
             <button
-              @click="$router.push({ path: '/upload' })"
+              @click="userRegister"
               class="block w-full px-6 py-4 text-lg font-semibold text-white rounded-full bg-orange-button hover:bg-green-button"
             >
               Continue Sign Up
@@ -71,7 +72,8 @@
         <div class="text-center">
           <p class="text-white text-md">
             Already have account?
-            <nuxt-link to="/login" class="no-underline text-orange-button">Sign In</nuxt-link
+            <nuxt-link to="/login" class="no-underline text-orange-button"
+              >Sign In</nuxt-link
             >.
           </p>
         </div>
@@ -83,6 +85,29 @@
 <script>
 export default {
   layout: 'auth',
+  data() {
+    return {
+      register: {
+        name: '',
+        email: '',
+        occupation: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    async userRegister() {
+      try {
+        let response = await this.$axios.post('/api/v1/users', this.register)
+        console.log(response.data.data.token)
+        this.$auth
+          .setUserToken(response.data.data.token)
+          .then(() => this.$router.push({ path: '/upload' }))
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
 }
 </script>
 
--- pages/upload.vue
+++ pages/upload.vue
@@ -3,9 +3,9 @@
     <div class="w-full px-10 lg:w-1/3 lg:px-0">
       <div class="flex items-center justify-center w-40 mx-auto mb-4">
         <div class="relative">
-          <a href="#">
+          <div class="cursor-pointer" @click="$refs.file.click()">
             <img
-              src="/avatar.jpg"
+              :src="url"
               alt=""
               class="border-4 border-white rounded-full"
             />
@@ -14,17 +14,20 @@
               alt=""
               class="absolute bottom-0 right-0 pb-2"
             />
-          </a>
+            <input type="file" ref="file" style="display: none" accept="image/*" @change="onFileChange">
+          </div>
         </div>
       </div>
       <h2 class="mb-3 text-3xl font-normal text-center text-white">
-        Hi, Brian
+        Hi, {{ this.$store.state.auth.user.name }}
       </h2>
       <p class="font-light text-center text-white">Please upload your selfie</p>
       <div class="mt-6 mb-4">
         <div class="mb-3 mx-9">
           <button
-            @click="$router.push({ path: '/register-success' })"
+            :disabled="selectedFiles ==undefined"
+            @click="upload"
+            :class="selectedFiles == undefined ? 'opacity-50 cursor-not-allowed' : ''"
             class="block w-full px-6 py-4 text-lg font-semibold text-white rounded-full bg-orange-button hover:bg-green-button"
           >
             Sign Up Now
@@ -48,5 +51,36 @@
 <script>
 export default {
   layout: 'auth',
+  data() {
+    return {
+      url: '/avatar.jpg',
+      selectedFiles: undefined,
+    }
+  },
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0]
+      this.url = URL.createObjectURL(file)
+      this.selectedFiles = this.$refs.file.files
+    },
+    async upload(file) {
+      let formData = new FormData()
+
+      formData.append('avatar', this.selectedFiles.item(0))
+
+      try {
+        let response = await this.$axios.post('/api/v1/avatars', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        console.log(response)
+
+        this.$router.push({ path: '/register-success' })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
 }
 </script>
